d79320b42f303f766a410f9c5d34fdadb291daa0
Clinton Ryan <clintryan3@gmail.com>
2016-11-28 17:03:03 +1100

    Adds new color theme and smallish tweak

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("4e753673a37c71b07e3026be75dc6af3efbac5ce335f3707b7d6a110ecb636a3" default)))
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "4e753673a37c71b07e3026be75dc6af3efbac5ce335f3707b7d6a110ecb636a3" default)))
  '(fci-rule-color "#383838")
  '(nrepl-message-colors
    (quote
